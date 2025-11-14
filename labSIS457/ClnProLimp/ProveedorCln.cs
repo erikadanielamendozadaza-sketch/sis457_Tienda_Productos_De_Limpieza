f3f2652bba654dbfc244b64231b953a2e4e3c084
danielhuata2-ship-it <danielhuata2@gmail.com>
2025-11-14 11:37:25 -0500

    Cln proveedor y unidad medida

--- labSIS457/ClnProLimp/ProveedorCln.cs
+++ labSIS457/ClnProLimp/ProveedorCln.cs
@@ -9,6 +9,50 @@ namespace ClnProLimp
 {
     public class ProveedorCln
     {
+
+        public static int insertar(Proveedor proveedor)
+        {
+            using (var context = new LabProLimpEntities())
+            {
+                context.Proveedor.Add(proveedor);
+                context.SaveChanges();
+                return proveedor.id;
+            }
+        }
+
+        public static int actualizar(Proveedor proveedor)
+        {
+            using (var context = new LabProLimpEntities())
+            {
+                var existe = context.Proveedor.Find(proveedor.id);
+                existe.nombreEmpresa = proveedor.nombreEmpresa;
+                existe.telefono = proveedor.telefono;
+                existe.direccion = proveedor.direccion;
+                existe.email = proveedor.email;
+                return context.SaveChanges();
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new LabProLimpEntities())
+            {
+                var existe = context.Proveedor.Find(id);
+                existe.estado = -1;
+                existe.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        public static Proveedor obtenerUno(int id)
+        {
+            using (var context = new LabProLimpEntities())
+            {
+                return context.Proveedor.Find(id);
+            }
+        }
+
+
         public static List<Proveedor> listar()
         {
             using (var context = new LabProLimpEntities())
@@ -16,5 +60,15 @@ namespace ClnProLimp
                 return context.Proveedor.Where(x => x.estado == 1).OrderBy(x => x.nombreEmpresa).ToList();
             }
         }
+
+        public static List<paProveedorListar_Result> listarPa(string parametro)
+        {
+            using (var context = new LabProLimpEntities())
+            {
+                return context.paProveedorListar(parametro).ToList();
+            }
+        }
+
+
     }
 }
--- labSIS457/ClnProLimp/UnidadMedidaCln.cs
+++ labSIS457/ClnProLimp/UnidadMedidaCln.cs
@@ -9,11 +9,60 @@ namespace ClnProLimp
 {
     public class UnidadMedidaCln
     {
+        public static int insertar(UnidadMedida unidadmedida)
+        {
+            using (var context = new LabProLimpEntities())
+            {
+                context.UnidadMedida.Add(unidadmedida);
+                context.SaveChanges();
+                return unidadmedida.id;
+            }
+        }
+
+        public static int actualizar(UnidadMedida unidadmedida)
+        {
+            using (var context = new LabProLimpEntities())
+            {
+                var existe = context.UnidadMedida.Find(unidadmedida.id);
+                existe.descripcion = unidadmedida.descripcion;
+                return context.SaveChanges();
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new LabProLimpEntities())
+            {
+                var existe = context.UnidadMedida.Find(id);
+                existe.estado = -1;
+                existe.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+            }
+                    
+        }
+
+
+        public static UnidadMedida obtenerUno(int id)
+        {
+            using (var context = new LabProLimpEntities())
+            { 
+                return context.UnidadMedida.Find(id);
+            }
+        }
+
         public static List<UnidadMedida> listar() 
         {
             using (var context = new LabProLimpEntities())
             {
                 return context.UnidadMedida.Where(x => x.estado == 1).OrderBy(x => x.descripcion).ToList();
+            }
+        }
+
+        public static List<paUnidadMedidaListar_Result> listarPa(string parametro)
+        {
+            using (var context = new LabProLimpEntities())
+            {
+                return context.paUnidadMedidaListar(parametro).ToList();
             }
         }
     }
